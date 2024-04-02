b1eaba611411a0b6e8af3ec0fe9216f7f22fa42d
Maksim Popov <maxim228maxim.sky@gmail.com>
2024-04-02 21:14:09 +0300

    Lol

--- TestOS/Kernel.cs
+++ TestOS/Kernel.cs
@@ -31,52 +31,7 @@ namespace TestOS
             Console.ForegroundColor = ConsoleColor.White;
             Console.WriteLine(input);
             Console.ResetColor();
-            /*
-            switch (input)
-            {
-                default:
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine(input + ": Check command");
-                    Console.ResetColor();
-
-                    break;
-                case "/help":
-                    Console.WriteLine("Hello, this is test OS!");
-                    Console.WriteLine("'poweroff' - shutdown PC");
-                    Console.WriteLine("'clear' - cleaning terminal");
-                    Console.WriteLine("'info' - show system info");
-                    break;
-                case "poweroff":
-                    for (int i = 3; i>0; i--)
-                    {
-                        Console.WriteLine("System will by shutdown by "+i);
-                        Thread.Sleep(1000);
-
-                    }
-                 
-                    Sys.Power.Shutdown();
-                    break;
-                case "reboot":
-                    for(int i = 3; i>0; i--)
-                    {
-                        Console.WriteLine("System will by shutdown by " + i);
-                        Thread.Sleep(1000);
-                    }
-                    Sys.Power.Reboot();
-                    break;
-                case "clear":
-                    Console.Clear();
-                    break;
-                case "info":
-                    string cpu_name = Cosmos.Core.CPU.GetCPUBrandString();
-                    uint ram_cap = Cosmos.Core.CPU.GetAmountOfRAM();
-
-                    Console.WriteLine("     _________                                         \r\n    / ======= \\                                        \r\n   / __________\\                                       \r\n  | ___________ |                                      \r\n  | | -       | |                                      \r\n  | |         | |                                      \r\n  | |_________| |________________________              \r\n  \\=____________/   OS on C#? Yes!     )               \r\n  / \"\"\"\"\"\"\"\"\"\"\" \\                       /              \r\n / ::::::::::::: \\                  =D-'               \r\n(_________________)                                    \r\n                                                       ");
-                    Console.WriteLine("CPU: "+ cpu_name);
-                    Console.WriteLine("MEM: " + (ram_cap) + "MB");
-                    break;
-
-            } */
+            
 
         }
     }

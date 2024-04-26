3fc672a822cda954bc44f467350a51c8fca43f08
Maksim Popov <115405460+3MtlFtnn@users.noreply.github.com>
2024-04-26 08:26:11 +0300

    Update Kernel.cs

--- TestOS/Kernel.cs
+++ TestOS/Kernel.cs
@@ -28,22 +28,6 @@ namespace TestOS
             Console.Clear();
             Console.WriteLine("Booted successfully.");
             this.ComManager = new ComManager();
-            /*
-            var available_space = vfs.GetAvailableFreeSpace(@"0:\");
-            var fs_type = vfs.GetFileSystemType(@"0:\");
-            Console.WriteLine("File System Type: " + fs_type);
-            Console.WriteLine("Available Free Space: " + available_space);
-            */
-            
-        }
-        //private string[] GetDirFadr(string adr) // get directories From address
-        //{
-
-           // string dirs = Directory.GetCurrentDirectory();
-
-            //return dirs;
-
-        //}
 
         protected override void Run()
         {
